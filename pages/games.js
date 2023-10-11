bbfab6adb17903af6fc1bbac34545ffd3fb0d0a0
CyclopPanda <106186023+CyclopPanda@users.noreply.github.com>
2023-10-11 19:44:31 +0100

    Update games.js

--- pages/games.js
+++ pages/games.js
@@ -1,5 +1,4 @@
 import styles from "../styles/Home.module.css";
-import BlogCard from "../components/BlogCard";
 import ParseContent from "../components/ParseContent";
 import DefaultHead from "../components/DefaultHead";
 
@@ -9,15 +8,12 @@ export async function getStaticProps() {
   return Content.getStaticProps();
 }
 
-export default function Home({ posts, layoutCategories}) {
-
-  var sortedPosts = ParseContent(posts, layoutCategories);
 
   return (
     <div className={styles.container}>
       <DefaultHead/>
 
-      <main className={styles.main}>
+      <main >
         <iframe frameborder="0" scrolling="no" src="https://mczak.com/code/sudoku/suframe/" style="border:0px solid #eee; overflow:hidden; width:400px; height:500px;" width="400"></iframe>
       </main>
 
